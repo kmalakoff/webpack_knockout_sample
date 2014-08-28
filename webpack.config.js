64a53d6ae87c5856e08deb7d9ad0910c8cf81a22
Kevin Malakoff <kevin@Kevins-MacBook-Pro.local>
2014-08-27 19:00:50 -0700

    Fix from: https://github.com/webpack/webpack/issues/424

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,12 @@ module.exports = {
     filename: 'app.js'
   },
 
+  module: {
+    loaders: [
+      {test: /knockout.build.output.knockout-latest\.js/, loader: 'imports?require=>false'}
+    ]
+  },
+
   resolve: {
     modulesDirectories: ['node_modules']
   }
